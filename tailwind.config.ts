a098040fb149a44d209b44ae4b18c78273c29a1a
Shreyashdeep <shreyashdeep20@gmail.com>
2024-10-16 23:21:13 +0530

    implement the steps to use this project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,10 +5,13 @@ const {nextui} = require("@nextui-org/react");
 const config: Config = {
     darkMode: ["class"],
     content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-	"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+		"./pages/**/*.{ts,tsx}",
+        "./components/**/*.{ts,tsx}",
+        "./app/**/*.{ts,tsx}",
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
   	extend: {
